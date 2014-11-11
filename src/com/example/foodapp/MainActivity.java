3246aab8313dc74bbd1953379626dc52017ecbc9
Jeff Walther <jwalther@insidesales.com>
2014-11-11 15:24:18 -0700

    Finished lousy tutorial.

--- src/com/example/foodapp/MainActivity.java
+++ src/com/example/foodapp/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.foodapp;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.*;
+import android.widget.*;
 
 
 public class MainActivity extends ActionBarActivity {
-
+	public final static String EXTRA_MESSAGE = "com.example.foodapp.Message";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,4 +34,15 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view){
+    	// Do something in response to the button
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
+    }
+
 }
